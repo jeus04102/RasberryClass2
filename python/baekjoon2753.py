28c923aa675801ee40aa90fc92504c2cb3ae69e9
jeus04102 <jeus04102@naver.com>
2022-03-30 22:22:44 +0900

    백준 2753번 윤년

--- python/baekjoon2753.py
+++ python/baekjoon2753.py
@@ -1,9 +1,7 @@
 import sys
 year= sys.stdin.readline()
 year= int(year)
-if year%4==0 and year%100!=0:
-    print("1")
-elif year%4==0 and year%400==0:
+if year%4==0 and year%100!=0 or year%400==0:
     print("1")
 else:
     print("0")
